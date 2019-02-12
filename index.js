f4aa9582f9dac93e3973702e058663206490ff13
Dmitriy Kuznetsov <d.o.kuznetsov@tinkoff.ru>
2019-02-12 10:49:50 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const fs = require('fs')
+const Module = require('module')
+const chalk = require('chalk')
+
+let singleton = false
+let errors = []
+
+const hook = () => {
+  if (singleton === true) {
+    throw new Error(`You could use ses only one time - in the root file`)
+  }
+
+  singleton = true
+  let i = 1
+
+  Module._extensions['.js'] = (module, filename) => {
+    const stack = ['']
+    let parent = module
+
+    while (parent) {
+      stack.push(parent.id)
+      parent = parent.parent
+    }
+
+    const source = fs.readFileSync(filename, 'utf8')
+
+    try {
+      module._compile(source, filename)
+    } catch (e) {
+      // Only first three lines, we dont need stacktrace with `cjs/loader.js:721:30`-like filenames
+      const originErr = e.stack.split('\n').slice(0, 3).join('\n')
+
+      errors.push(
+        chalk.red(`\nBad require chain #${i++}`) +
+        chalk.blue(stack.join('\n  → ') + '\n\n') +
+        chalk.grey(originErr) + '\n'
+      )
+
+      delete require.cache[filename]
+    }
+  }
+}
+
+hook.errors = () => errors
+
+module.exports = (absPath) => {
+  hook()
+
+  require(absPath)
+
+  const errors = hook.errors()
+
+  if (!errors.length) {
+    console.log('SES OK!')
+  } else {
+    console.log(errors.join(''))
+    console.error(`Failed to 'require(${absPath})'`)
+    throw new Error(`Failed to check!`)
+  }
+}
